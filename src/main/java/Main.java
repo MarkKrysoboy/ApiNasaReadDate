3e839f23492535dfeed6509b35c49964af1c530c
Mark Krysoboy <heter1209@gmail.com>
2022-01-26 17:14:39 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    public static final String PATH_SERVER_URL = "https://api.nasa.gov/planetary/apod?" +
+            "api_key=8tJEtMJJZtODzPYhrbNqkP6fttZgaREERvnUSqyr";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        try(CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build()){
+            HttpGet request = new HttpGet(PATH_SERVER_URL);
+            CloseableHttpResponse response = httpClient.execute(request);
+
+//            Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+//            String answer = new String(response.getEntity().getContent().readAllBytes());
+//            System.out.println(answer);
+
+            NasaAnswer nasaAnswer = mapper.readValue(response.getEntity().getContent().readAllBytes(),
+                    new TypeReference<>() {
+            }
+            );
+
+            String url = nasaAnswer.getUrl();
+            request = new HttpGet(url);
+            String fileName = url.substring(url.lastIndexOf("/")+1);
+            response = httpClient.execute(request);
+
+            File file = new File(fileName);
+            try (FileOutputStream fos = new FileOutputStream(file);
+                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+                byte[] bytes = response.getEntity().getContent().readAllBytes();
+                bos.write(bytes, 0, bytes.length);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
